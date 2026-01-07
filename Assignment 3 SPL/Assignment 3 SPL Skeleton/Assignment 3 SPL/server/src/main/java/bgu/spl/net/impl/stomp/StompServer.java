96cd609e571b9f2d8ad6db2499ae8795cabd3851
mohamad-911 <mohamadj@post.bgu.ac.il>
2026-01-07 10:30:10 +0000

    Changed

--- Assignment 3 SPL/Assignment 3 SPL Skeleton/Assignment 3 SPL/server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
+++ Assignment 3 SPL/Assignment 3 SPL Skeleton/Assignment 3 SPL/server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
@@ -1,8 +1,33 @@
 package bgu.spl.net.impl.stomp;
 
+import bgu.spl.net.srv.Server;
+
 public class StompServer {
 
     public static void main(String[] args) {
         // TODO: implement this
+        // Added by us
+        // you can use any server...
+        // String [] args={"2000","tpc"};
+        // args[0]="2000";
+        // args[1]="reactor";
+        if(args[1].equals("tpc")){
+        Server.threadPerClient(
+            Integer.parseInt(args[0]), // port
+                () -> new StompProtocol(), // protocol factory
+                StompMessageEncoderDecoder::new // message encoder decoder factory ///////
+        ).serve();
+
+        }else if(args[1].equals("reactor")){
+        Server.reactor(
+        Runtime.getRuntime().availableProcessors(),
+        Integer.parseInt(args[0]), //port
+        () -> new StompProtocol(), //protocol factory
+        StompMessageEncoderDecoder::new //message encoder decoder factory
+        ).serve();
+        }else{
+            System.out.print("something wrong!!");
+        }
+        // Adding end
     }
 }
