68fc4303cb5e7a0fc95a6fcc562d4e6ab3ed9145
mohamad-911 <mohamadj@post.bgu.ac.il>
2026-01-07 10:17:29 +0000

    Changed

--- Assignment 3 SPL/Assignment 3 SPL Skeleton/Assignment 3 SPL/client/include/StompProtocol.h
+++ Assignment 3 SPL/Assignment 3 SPL Skeleton/Assignment 3 SPL/client/include/StompProtocol.h
@@ -1,10 +1,57 @@
 #pragma once
+#pragma once
+#include <stdlib.h>
+#include <string>
+#include <iostream>
+#include <map>
+#include "ConnectionHandler.h"
+#include "../include/event.h"
+#include <vector>
+using std::cerr;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::map;
 
 #include "../include/ConnectionHandler.h"
 
 // TODO: implement the STOMP protocol
 class StompProtocol
+// Added by us 
 {
 private:
+    string command;
+    string CountinueMsg;
+    map<int ,string> ChannelsSubscHashMap;
+    int Subscribtion_ID;
+    int Subscribtion_Recipt;
+    string CommandToServer;
+    string UserName;
+    int Subid;
+    int Unsubid;
+    int Disid;
+    string Channel;
+    
 public:
+    map<string ,map<string,std::vector<string>>> MapForSummary;
+    StompProtocol(string JustForNotDefault);
+    virtual ~StompProtocol();
+    string CONNECT();
+    string SUBSCRIBE();
+    string UNSUBSCRIBE();
+    string SEND();
+    string DISCONNECT();
+    string SolveCommand(string Command,string Msg);
+    string GetMessage();
+    string printHashMaps(map<string,string> hash);
+    std::vector<string> ReportEvents(std::vector<Event> events);
+    void SummaryPrepare(string Respond);
+    string checkIfSubscribe(string channel);
+    int getSubid();
+    int getUnsubid();
+    int getDisid();
+    string getChannel();
+    void setChannel(string str);
+// Adding end
 };
